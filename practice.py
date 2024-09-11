3a88b0804397c2e9ae336c81565a5b59ce26c84d
eshajosyula <gayatri.josyula@gmail.com>
2024-09-11 10:33:52 -0400

    mini practicum completed

---
+++ practice.py
@@ -0,0 +1,24 @@
+def convert_height():
+    inches = int(input("Enter your height in inches: "))
+    #print(inches)              "troubleshooting"
+    feet = int(inches/12)
+    #print(feet)              "troubleshooting"
+    inc = int(inches % 12)
+    #print(inc)              "troubleshooting"
+    print("You are ", feet, "' ", inc, "''" , " tall", sep = "")
+
+def before(letter):
+    now = ord(letter)
+    prev1 = chr(now - 1)
+    prev2 = chr(now - 2)
+    prev3 = chr(now - 3)
+
+    print(letter, prev1, prev2, prev3, sep = "\n")
+    
+
+def main():
+    convert_height()
+    #before("X")
+    #print(71%(int(71/12)))
+
+main()
