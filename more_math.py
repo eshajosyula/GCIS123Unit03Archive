603bc7e35f10083d694188da30f7dcfcf30b40e5
eshajosyula <gayatri.josyula@gmail.com>
2024-09-15 22:12:45 -0400

    first commit more_math.py

---
+++ more_math.py
@@ -0,0 +1,22 @@
+import math
+
+def fact(num):
+    if num > 0:
+        return math.factorial(num)
+    else:
+        return 0
+    
+def root(num):
+    if num > 0:
+        return float(math.sqrt(num))
+    else: 
+        return 0
+    
+def trunk(num):
+    return float(math.trunc(num))
+
+def main():
+    x = int(input("input a number: "))
+    print(fact(x), "\n", root(x), "\n", trunk(x), sep = "")
+
+main()
